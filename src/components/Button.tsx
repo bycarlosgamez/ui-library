f3eae8220ce8fcea5dd79727ba9abc10b377b220
bycarlosgamez <hardpx@gmail.com>
2026-01-06 12:35:18 -0600

    added button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,24 +2,31 @@ import { mergeProps, useRender } from '@base-ui/react';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { twMerge } from 'tailwind-merge';
 
-const buttonVariants = cva('inline-flex justify-center, items-center', {
-  variants: {
-    variant: {
-      default: '',
-      primary: 'bg-brand-blue-400',
-      destructive: 'bg-red-500',
-      ghost: '',
+const buttonVariants = cva(
+  'inline-flex justify-center items-center cursor-pointer hover:opacity-80 focus-visible:ring-highlight/30 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none border border-transparent text-white',
+  {
+    variants: {
+      variant: {
+        default: 'bg-primary',
+        secondary: 'bg-primary/10 text-foreground',
+        outline: 'border-primary bg-primary/20 text-foreground',
+        ghost: 'bg-primary/0 hover:bg-primary/10 text-foreground',
+        destructive: 'bg-destructive/10 text-destructive ',
+        link: 'text-foreground hover:underline',
+      },
+      size: {
+        default: 'h-10 px-4 rounded-lg',
+        sm: 'h-8 px-3 rounded-md text-sm',
+        lg: 'h-12 px-6 rounded-xl text-lg',
+        icon: 'h-12 w-12 rounded-lg',
+      },
     },
-    size: {
-      default: 'h-12 px-4',
-      icon: 'h-12 w-12',
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
     },
   },
-  defaultVariants: {
-    variant: 'default',
-    size: 'default',
-  },
-});
+);
 
 interface ButtonProps
   extends
